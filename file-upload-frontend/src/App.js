7b2304c785f2537e07ca44a2bd7ff69ae6eb6223
tatianasabanina <tatiana08st@gmail.com>
2021-06-24 15:52:34 +0300

    Fix a bug: the page header is invisible if div height exceeds the view heigth

--- file-upload-frontend/src/App.js
+++ file-upload-frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Grid, ThemeProvider } from '@material-ui/core/';
+import {
+ Grid, ThemeProvider, Toolbar 
+} from '@material-ui/core/';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -15,16 +17,16 @@ function App() {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <Grid container direction="column" alignItems="center" justify="space-between" className="wrapper">
-          <Grid container item>
-            <Navbar />
-          </Grid>
-          <Grid container item style={{ flexGrow: 1 }}>
+        <Grid container direction="column" justify="center" className="root">
+          <Navbar />
+          <Toolbar />
+          <Grid item xs={12}>
             <Route path="/" exact component={LocalUpload} />
             <Route path="/second" exact component={CloudUpload} />
             <Route path="/third" exact component={ThirdPage} />
+         
+            <Footer />
           </Grid>
-          <Footer />
         </Grid>
       </ThemeProvider>
     </Router>
